5392d558d1cd89609584f7e68b115d07644470d5
garg-akash <akashgrg2@gmail.com>
2018-02-24 13:08:06 +0530

    Linearization Points in terms of velocity

--- MPC_2d_staticObs.m
+++ MPC_2d_staticObs.m
@@ -1,6 +1,6 @@
 %%IT is for 2d only
-% eucldFunc(), tc
-% p <= n
+% p <= n; for increased planning steps 
+%jerk cost added
 clc;
 clear all;
 close all;
@@ -13,7 +13,7 @@ del_t = 0.1; % Time duration for which some command is executed.
 
 %% defining drone uncertainty
 % drone_sig_orig = [0.02, 0.0, 0.0; 0.0, 0.02, 0.0; 0.0, 0.0, 0.02];
-% drone_sig_orig = [0.02, 0.0; 0.0, 0.02];
+% drone_sig_orig = [0.02, 0.0; + jerk_cost0.0, 0.02];
 
 %% Defining obstacle trajectory
 %% For now we will assume that obstacle is moving along a straing line. it's starting from (0,0,0) and It's trajectory will be determined based on the destination of the drone
@@ -26,10 +26,21 @@ Py_not = start_y;
 dest_x = 40;
 dest_y = 0;
 
-wp_ind = [1,0,20; 2,0,40; 3,0,60; 4,0,80; 5,0,100; 6,0,120; 7,0,140; 8,0,160; 9,0,180; 10,0,200; 
-    11,0,220; 12,0,240; 13,0,260; 14,0,280; 15,0,300; 16,0,320; 17,0,340; 18,0,360; 19,0,380; 20,0,400;
-    21,0,420; 22,0,440; 23,0,460; 24,0,480; 25,0,500; 26,0,520; 27,0,540; 28,0,560; 29,0,580; 30,0,600;
-    31,0,620; 32,0,640; 33,0,660; 34,0,680; 35,0,700; 36,0,720; 37,0,740; 38,0,760];
+% wp_ind = [1,0,20; 2,0,40; 3,0,60; 4,0,80; 5,0,100; 6,0,120; 7,0,140; 8,0,160; 9,0,180; 10,0,200; 
+%     11,0,220; 12,0,240; 13,0,260; 14,0,280; 15,0,300; 16,0,320; 17,0,340; 18,0,360; 19,0,380; 20,0,400;
+%     21,0,420; 22,0,440; 23,0,460; 24,0,480; 25,0,500; 26,0,520; 27,0,540; 28,0,560; 29,0,580; 30,0,600;
+%     31,0,620; 32,0,640; 33,0,660; 34,0,680; 35,0,700; 36,0,720; 37,0,740; 38,0,760; 39,0,780];
+
+mit_dist = 39;
+mit_inter_dist = 0.25;
+mit_avg_speed = 0.5;
+mit_x = 1:mit_inter_dist:mit_dist;
+mit_step = mit_inter_dist/(mit_avg_speed*del_t);
+mit_y = linspace(0,0,size(mit_x,2));
+mit_r = mit_step:mit_step:(size(mit_x,2))*mit_step;
+wp_ind = [mit_x',mit_y',mit_r'];
+
+
 
 i = 1;
 ln = 0;
@@ -47,57 +58,24 @@ while (i<=size(wp_ind,1))
 end
 
 %%%%%%%%%%%%%% Obstacle stuff starts here!!!! %%%%%%%%%%%%%%%%%
-% Ob_start=[0, dest_y/2, dest_z];
-% Ob_end=[dest_x, dest_y/2, 0];
-% Ob_start=[-dest_z/3, 0, dest_x/3];                              %%Meeting at 1/3
-% Ob_end=[dest_x + 2*dest_z/3, dest_y, dest_z - 2*dest_x/3];
-
-Ob_start=[6,0];
+Ob_start=[4,0];
 Obx_not = Ob_start(1);
 Oby_not = Ob_start(2);
 % Ob_end=[start_x,start_y,start_z];
 
 %% Calculating velocity of the obstacle in all the directions
-%vel_obs = (Ob_end - Ob_start)/tot_time;
 vel_obs = 0;
 theta_obs = -pi;
 
-
-%% Defining obstalce uncertainty
-% obs_sig_orig = [0.02, 0.0;0.0, 0.02];
-
-%% Criterion threshold
-% crit_thresh =  0.0000001;
-
-%% The minimum confidence by which both, the drone and obstacle should be avoided
-% confidence_low = 0.30;
-% confidence_e = 0.99;
-
-% load Linear_approx_3d.mat
-%% Finding U1 threshold for the above confidence
-% P_u1 = Overlap(int16(confidence_low*length(Conf)))/2;
-% CP_u1 = 1 - P_u1; %% Cumulative probability of U1
-% u1_thresh = norminv(CP_u1);
-
-%% Blowing covariance  in proportion of the radius of the obstacle and drone
 R_obs = 0.5;
 R_drone = 0.5;
-% obs_sig = blow_sigma(obs_sig_orig, confidence_low, R_obs);
-% drone_sig = blow_sigma(drone_sig_orig, confidence_low, R_drone);
-% 
-% obs_sig_e = blow_sigma(obs_sig_orig, confidence_e, R_obs);
-% drone_sig_e = blow_sigma(drone_sig_orig, confidence_e, R_drone);
+
 %%%%%%%%%Obstacle stuff ends here %%%%%%%%%%%%%%%
 
 %% Difference between subsequent velocities
-del_Vx = 2*0.5;
-del_Vy = 2*0.5;
+del_Vx = 0.5;
+del_Vy = 0.5;
 % del_Vz = 0.5;
-
-%% Initializing linearization point for ts
-% for i = 1:n
-% 	td(i) = 0.5;
-% end
 
 dist = 1;
 c = 0;
@@ -108,6 +86,8 @@ Ox_arr = [];
 Oy_arr = [];
 Vx_arr = [];
 Vy_arr = [];
+ax = 3;
+ay = 2;
 
 while (dist>0.8)
     l = 0;
@@ -117,6 +97,7 @@ while (dist>0.8)
     while (myflag)
         qn = 0;
         cost = 0;
+        jerk_cost = 0;
         j = [];  %%To track WP in planning range
         cvx_begin quiet
         clear Vx Vy Vz Px Py Pz
@@ -149,18 +130,16 @@ while (dist>0.8)
             if (0< wp_ind(i,3) && wp_ind(i,3)<=n)       %%Don't put equals to 0
                 j = [j;i];
                 cost = cost + (Px(wp_ind(i,3),1) - wp_ind(i,1))^2 + (Py(wp_ind(i,3),1) - wp_ind(i,2))^2;
+%                 if l > 0
+%                     for q = 2:(n-1)
+%                         jerk_cost = jerk_cost + ((-Vx(q-1) + 2*Vx(q) - Vx(q+1))/(del_t)^2)^2 + ((-Vy(q-1) + 2*Vy(q) - Vy(q+1))/(del_t)^2)^2;
+%                     end
+%                 end
             end
-%             wp_ind(i,3) = wp_ind(i,3) - 1;
         end
         minimise(cost)
         
-        %%Obstacle avoindace constrains
-        %% original constraint is as follows
-        %% (Px(i) - Ox(i))^2/(sig_x^2) + (Py(i) - Oy(i))^2/(sig_y^2) + (Pz(i) - Oz(i))^2/(sig_z^2) >= R^2
-        %% Since the above constraint is quadratic in nature, we will linearize it around point x(i), y(i), z(i)
-        %% f(Px(i),Py(i),Pz(i)) = (Px(i) - Ox(i))^2 + (Py(i) - Oy(i))^2 + (Pz(i) - Oz(i))^2 linearized around x(i), y(i), z(i) will look like following
-        %% (x(i) - Ox(i))^2 + (y(i) - Oy(i))^2 + (z(i) - Oz(i))^2 + 2(x(i) - Ox(i))*(Px(i) -  Ox(i)) + 2(y(i) - Oy(i))*(Py(i) -  Oy(i)) + 2(y(i) - Oy(i))*(Py(i) -  Oy(i))
-        %% Constraint will be (x(i) - Ox(i))^2 + (y(i) - Oy(i))^2 + (z(i) - Oz(i))^2 + 2(x(i) - Ox(i))*(Px(i) -  Ox(i)) + 2(y(i) - Oy(i))*(Py(i) -  Oy(i)) + 2(z(i) - Oz(i))*(Pz(i) -  Oz(i)) > R^2
+        %%Constraint for onstacle avoidance
         if(l~=0)
             fprintf('Distance: %f l: %d\n',sqrt((x(1,1) - Ox(1,1))^2 + (y(1,1) - Oy(1,1))^2),l)
             %if (eucldFunc(Px(find(Px(:,2))==tc), Py(find(Py(:,2))==tc), Ox(find(Ox(:,2))==tc), Oy(find(Oy(:,2))==tc))< sense)
@@ -170,19 +149,15 @@ while (dist>0.8)
                 end
                 fprintf('Entered!!\n');
                 fprintf('l: %d R: %d\n',l,R);
-                if (R<1)
-                    R = R + 0.2;
-                end
+%                 if (R<0.2)
+%                     R = R + 0.05;
+%                 end
             end
         end
         %%%%%%%% Constraints %%%%%%%%
-        Vx >= 0;
-        %         Vy >= 0;
-        %     Vz >= 0;
-        
-        %% Constraint on t
-        %     0 <= t <= 1;
-        
+        Vx <= 1;
+        Vy <= 1;
+       
         %% The velocity during initialization shouldn't be much higher
 %         0 <= Vx(1) <= del_Vx;
 %         -del_Vy <= Vy(1) <= del_Vy;
@@ -193,10 +168,15 @@ while (dist>0.8)
         %     0 <= Vz(n) <= 1.0;
         
         %	% Ensuring that subsequent X vel commands do not have difference more than del_Vx
-        0 <= Vx(2:n) - Vx(1:n-1) <= del_Vx;
+
+        -ax*del_t <= Vx(2:n) - Vx(1:n-1);
+        
+        Vx(2:n) - Vx(1:n-1) <= ax*del_t;
         
         %% Ensuring that subsequent Y vel commands do not have difference more than del_Vy
-        -del_Vy <= Vy(2:n) - Vy(1:n-1) <= del_Vy;
+        -ay*del_t <= Vy(2:n) - Vy(1:n-1);
+        
+        Vy(2:n) - Vy(1:n-1) <= ay*del_t;
         
         %% Ensuring that subsequent Z vel commands do not have difference more than del_Vz
         %     0 <= Vz(2:n) - Vz(1:n-1) <= del_Vz;
@@ -207,24 +187,17 @@ while (dist>0.8)
         fprintf('Vx1:%f Vx2:%f Vx3:%f\n',Vx(1),Vx(2),Vx(3));
         fprintf('Vy1:%f Vy2:%f Vy3:%f\n\n',Vy(1),Vy(2),Vy(3));
         %% Updating the linearization points!
-        x = Px;
-        y = Py;
-        %     z = Pz;
-        %cvx_optval
-        %     if(l ~= no_of_iter-1)
-        %         clear Vx Vy Vz Px Py Pz
-        %     end
-        %     if(l>1)
-        %     	td = t;
-        %     	if confidence_low < 0.9
-        %     		confidence_low = confidence_low + 0.1;
-        %     	end
-        % Finding U1 threshold for the above confidence
-        % 	P_u1 = Overlap(int16(confidence_low*length(Conf)))/2;
-        % 	CP_u1 = 1 - P_u1; %% Cumulative probability of U1
-        %     u1_thresh = norminv(CP_u1);
-        % end
+        git_x = double(Vx);
+        git_y = double(Vy);
         
+        x = Px_not + cumsum(git_x*del_t);
+        y = Py_not + cumsum(git_y*del_t);
+        if(l == 0)
+            x = x + 0.1*rand(n,1);
+            y = y + 0.1*rand(n,1);
+        end
+        %     z = Pz;
+   
         l = l + 1;
      
         if ((cvx_optval - prev_optval) < 0.1 && l>4)
@@ -236,14 +209,15 @@ while (dist>0.8)
         prev_optval = cvx_optval;
         
         figure(4);
+        cla;
         hold on;
         axis equal;
-        p1 = plot(Px,Py,'r','LineWidth',2);
+        p1 = plot(Px(:,1),Py(:,1),'r','LineWidth',2);
         hold on
         p2 = plot(wp_ind(j,1), wp_ind(j,2),'o','MarkerEdgeColor','b');
         pause(1);
-        delete(p1);
-        delete(p2);
+%         delete(p1);
+%         delete(p2);
         
         %R = R + 1
     end
@@ -269,8 +243,6 @@ while (dist>0.8)
  
     Vx_arr = [Vx_arr;Vx(1)];
     Vy_arr = [Vy_arr;Vy(1)];
-%     fprintf('Vx: %f \n',Vx(1));
-%     fprintf('Vy: %f \n',Vy(1));
     
     dist = sqrt((Px(1)-dest_x)^2 + (Py(1)-dest_y)^2);
 end %% End of while loop
